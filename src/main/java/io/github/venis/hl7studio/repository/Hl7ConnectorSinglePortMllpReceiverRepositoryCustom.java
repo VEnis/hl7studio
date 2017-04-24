be3bb89f0eab69500415f96044957a058bc75625
Vyacheslav Enis <venis@difane.com>
2017-04-24 15:05:42 +0300

    Added repositories

---
+++ src/main/java/io/github/venis/hl7studio/repository/Hl7ConnectorSinglePortMllpReceiverRepositoryCustom.java
@@ -0,0 +1,13 @@
+package io.github.venis.hl7studio.repository;
+import io.github.venis.hl7studio.model.Hl7ConnectorSinglePortMllpReceiver;
+import org.springframework.roo.addon.layers.repository.jpa.annotations.RooJpaRepositoryCustom;
+
+/**
+ * = Hl7ConnectorSinglePortMllpReceiverRepositoryCustom
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooJpaRepositoryCustom(entity = Hl7ConnectorSinglePortMllpReceiver.class)
+public interface Hl7ConnectorSinglePortMllpReceiverRepositoryCustom {
+}
---
+++ src/main/java/io/github/venis/hl7studio/repository/Hl7MessageRepositoryCustom.java
@@ -0,0 +1,13 @@
+package io.github.venis.hl7studio.repository;
+import io.github.venis.hl7studio.model.Hl7Message;
+import org.springframework.roo.addon.layers.repository.jpa.annotations.RooJpaRepositoryCustom;
+
+/**
+ * = Hl7MessageRepositoryCustom
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@RooJpaRepositoryCustom(entity = Hl7Message.class)
+public interface Hl7MessageRepositoryCustom {
+}
